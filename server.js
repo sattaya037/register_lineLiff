ad831558ce00a0afac6a74e5696827190467a3e3
sattaya037 <sattaya.ra@ics-th.com>
2019-11-20 09:52:52 +0700

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const myLiffId = process.env.MY_LIFF_ID;
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(__dirname));
+    // res.sendFile(__dirname+'/holiday.html')
 
 app.get('/send-id', function(req, res) {
     res.json({id: myLiffId});
