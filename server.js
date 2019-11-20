49841039824b727dc8ef565f5363a32e78065839
sattaya037 <sattaya.ra@ics-th.com>
2019-11-20 09:58:38 +0700

    make it better

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
 
-// app.use(express.static('public'));
-app.use(express.static(__dirname));
-res.sendFile(__dirname+'/liff.html')
+app.use(express.static('public'));
 
 app.get('/send-id', function(req, res) {
     res.json({id: myLiffId});
 });
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`listening on ${port}`);
-  });
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
