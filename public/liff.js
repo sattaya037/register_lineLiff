f7faca334a10576061036914b983350326e189b3
sattaya037 <sattaya.ra@ics-th.com>
2019-11-20 15:14:50 +0700

    make it better

--- public/liff.js
+++ public/liff.js
@@ -92,9 +92,9 @@ function displayIsInClientInfo() {
     if (liff.isInClient()) {
         document.getElementById('liffLoginButton').classList.toggle('hidden');
         document.getElementById('liffLogoutButton').classList.toggle('hidden');
-        document.getElementById('isInClientMessage').textContent = 'You are opening the app in the in-app browser of LINE.';
+        // document.getElementById('isInClientMessage').textContent = 'You are opening the app in the in-app browser of LINE.';
     } else {
-        document.getElementById('isInClientMessage').textContent = 'You are opening the app in an external browser.';
+        // document.getElementById('isInClientMessage').textContent = 'You are opening the app in an external browser.';
     }
 }
 
