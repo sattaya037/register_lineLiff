52ff49394f05c2c9360d6c904f53080de2ac9962
sattaya037 <sattaya.ra@ics-th.com>
2019-11-25 14:58:56 +0700

    first commit

--- vote/vote.js
+++ vote/vote.js
@@ -103,7 +103,7 @@ function firebaseHandlers() {
       firebase.initializeApp(firebaseConfig);
       firebase.analytics();
       const dbRef = firebase.database().ref('HPY');
-      dbRef.on("value", function(snapshot) {
+      dbRef.on("child_added", function(snapshot) {
         console.log(snapshot.val());
         document.getElementById("demo").innerHTML = snapshot.key;
 
