dd99923b4b55d20b1323e915534a467fe528b7dc
sattaya037 <sattaya.ra@ics-th.com>
2019-11-26 15:41:06 +0700

    first commit

--- vote/vote.js
+++ vote/vote.js
@@ -142,7 +142,7 @@ function displayIsInClientInfo() {
             document.getElementById('liffLogoutButton').classList.toggle('hidden');
             document.getElementById('displaynamefield').innerHTML=profile.displayName;
             document.getElementById("image").src=profile.pictureUrl; 
-          
+            console.log('profile');
         })
         .catch((err) => {
           console.log('error', err);
