4dc17758163dcaeabcb88c446b05949aec1fc012
sattaya037 <sattaya.ra@ics-th.com>
2019-11-28 12:05:39 +0700

    first commit

--- vote/vote.js
+++ vote/vote.js
@@ -121,7 +121,7 @@ function PromiseHandlers(dbRef,lineID) {
     var promise1 = new Promise(function(resolve, reject) {
         dbRef.on("child_added", function(snapshot) {
             var voteValue = snapshot.key;
-            console.log(snapshot.val().result)
+            console.log(snapshot.val().result.lineName)
             var check=[];
             // var key = dbRef.child(voteValue).child("result");
             // key.orderByKey().equalTo("test").once("value", function(snapshot){
