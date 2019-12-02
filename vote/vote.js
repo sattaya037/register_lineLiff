390470494f1cc4a558e8eb3c8ac4c4e1b8cb4e34
sattaya037 <sattaya.ra@ics-th.com>
2019-12-02 15:08:33 +0700

    first commit

--- vote/vote.js
+++ vote/vote.js
@@ -121,9 +121,10 @@ window.onload = function() {
                 button ='<button id="'+snapshot.key+'" onClick="Vote(this.id)" type="button" data-toggle="modal" data-target="#voteModel" class="btn btn-primary">Vote</button>';
   
             }         
-            content +='<li class="list-group-item">'+snapshot.key;
             content +='<span class="badge badge-primary badge-pill">14</span>';
             content +='<div class="float-right">'+button+'</div>';
+            content +='<li class="list-group-item">'+snapshot.key;
+
             content +'</li>';
             // content +='<div class="card">';
             // // content +='<img class="card-img-top"'; 
