87b0d74ee95d49b04f37d1d29ce08136a32a23fc
sattaya037 <sattaya.ra@ics-th.com>
2019-12-04 14:31:46 +0700

    make it better

--- vote/vote.js
+++ vote/vote.js
@@ -118,12 +118,11 @@ window.onload = function() {
   
   
             }else{
-                button ='<button id="'+snapshot.key+'" onClick="Vote(this.id)" type="button" data-toggle="modal" data-target="#voteModel" class="btn btn-primary">Vote <span class="badge badge-light">'+count+'</span> </button>';
-  
+                button ='<button id="'+snapshot.key+'" onClick="Vote(this.id)" type="button" data-toggle="modal" data-target="#voteModel" class="list-group-item list-group-item-action">Vote <span class="badge badge-light">'+count+'</span> </button>';
+
             }         
-            content +='<li class="list-group-item">'+snapshot.key;
-            content +='<div class="float-right">'+button+'</div>';
-            content +='</li>'; 
+            content +=button;
+         
             // content +='<div class="card">';
             // // content +='<img class="card-img-top"'; 
             // // content +=  'src='+snapshot.val().image +'alt="Card image cap">';
