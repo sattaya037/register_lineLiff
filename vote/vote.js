e622509bcc131ba51d4c90a035c75c641936ae40
sattaya037 <sattaya.ra@ics-th.com>
2019-12-26 17:29:27 +0700

    make it better

--- vote/vote.js
+++ vote/vote.js
@@ -153,16 +153,20 @@ function render(arrObj){
   
   sortedArr = arrObj.sort();
   var content2 ='';
+
   for (var i = 0; i < sortedArr.length; i = i + count) {
     count = 1;
     for (var j = i + 1; j < sortedArr.length; j++) {
+
       if (sortedArr[i] === sortedArr[j])
         count++;
+
     }
-         content2 +='<div class="progress">';
-         content2 +='<div class="progress-bar" role="progressbar" style="width:'+count+'%;" aria-valuenow="'+count+'" aria-valuemin="0" aria-valuemax="'+arrObj.length+'">'+sortedArr[i]+'</div>';
-         content2 +='</div>';
-         content2 +='<br>';
+      
+    content2 +='<div class="progress">';
+    content2 +='<div class="progress-bar" role="progressbar" style="width:'+count+'%;" aria-valuenow="'+count+'" aria-valuemin="0" aria-valuemax="'+arrObj.length+'">'+sortedArr[i]+'</div>';
+    content2 +='</div>';
+    content2 +='<br>';
 
     var theDiv2 = document.getElementById("output");
     theDiv2.innerHTML += content2; 
