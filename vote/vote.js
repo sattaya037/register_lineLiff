a72083d52a34e4c9010663177c5b8dded948381c
sattaya037 <sattaya.ra@ics-th.com>
2019-12-26 14:58:04 +0700

    make it better

--- vote/vote.js
+++ vote/vote.js
@@ -137,7 +137,7 @@ function vote(value){
 
 function view(){
     const voteRef = firebase.database().ref('HPY/voters')
-    voteRef.on("child_moveds", function(snapshot) {
+    voteRef.on("child_moved", function(snapshot) {
         console.log(snapshot.val())
     })
 
