1958ef7c0d00a46e50403fe3c9211070fbbb6498
sattaya037 <sattaya.ra@ics-th.com>
2019-12-26 13:32:10 +0700

    make it better

--- vote/vote.js
+++ vote/vote.js
@@ -120,7 +120,7 @@ function vote(value){
             console.log(childsnapshot.val())
              console.log(childsnapshot.key)
              var snapKey =childsnapshot.key;
-            content +='<button id="'+childsnapshot.key+'" onclick="onSubmit(\'' + snapKey + '\')" class="w3-button w3-black" value="'+childsnapshot+'" >'+childsnapshot.key+'</button>';
+            content +='<button id="'+childsnapshot.key+'" onclick="onSubmit(\'' + childsnapshot + '\')" class="w3-button w3-black" value="'+childsnapshot+'" >'+childsnapshot.key+'</button>';
 
         })
         var theDiv = document.getElementById("btn");
@@ -130,10 +130,9 @@ function vote(value){
     console.log(dbRef)
 }
 
-function onSubmit(snapKey){
+function onSubmit(childsnapshot){
     var a =
-    console.log("tt")
-    console.log(snapKey)
+    console.log(snachildsnapshotpKey)
 
 }
   
