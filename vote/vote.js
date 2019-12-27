e9337a32a1f51b07b2c09fffa29e4e3a58a49365
sattaya037 <sattaya.ra@ics-th.com>
2019-12-27 10:27:39 +0700

    make it better

--- vote/vote.js
+++ vote/vote.js
@@ -161,7 +161,7 @@ function render(arrObj){
     var ctx = document.getElementById('myChart').getContext('2d');
     var chart = new Chart(ctx, {
         // The type of chart we want to create
-        type: 'line',
+        type: 'pie',
     
         // The data for our dataset
         data: {
