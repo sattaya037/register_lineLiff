d574dd7e7e9daa0ffb5ca5e3f699f5848ee8a13f
sattaya037 <sattaya.ra@ics-th.com>
2019-12-27 11:12:42 +0700

    make it better

--- vote/vote.js
+++ vote/vote.js
@@ -173,7 +173,7 @@ function render(arrObj){
         data: {
             labels: data1,
             datasets: [{
-                label: 'My First dataset',
+                label: data1,
                 backgroundColor: ["#0074D9", "#FF4136", "#2ECC40", "#FF851B", "#7FDBFF", "#B10DC9", "#FFDC00", "#001f3f", "#39CCCC", "#01FF70", "#85144b", "#F012BE", "#3D9970", "#111111", "#AAAAAA"],
                 borderColor: 'rgb(255, 99, 132)',
                 data:data2
