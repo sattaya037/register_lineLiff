fbcd0c5060700a3cdef558bee0069e4f310c1071
sattaya037 <sattaya.ra@ics-th.com>
2019-12-27 14:27:30 +0700

    make it better

--- test/index.js
+++ test/index.js
@@ -21,7 +21,7 @@
   fetch(url)
   .then((resp) => resp.json())
   .then(function(data) {
-    console.log(data)
+    console.log(data.data)
   })
   .catch(function(error) {
     console.log(JSON.stringify(error));
