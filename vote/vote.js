2040a3c7ad102bb293d79a6883963ab861b10596
sattaya037 <sattaya.ra@ics-th.com>
2019-12-27 09:37:25 +0700

    make it better

--- vote/vote.js
+++ vote/vote.js
@@ -161,8 +161,10 @@ function render(arrObj){
     document.getElementById('output').innerHTML = Object.keys(obj).map(function(item) {
         var content2 = '';
          content2 +='<div class="progress">';
-         content2 +='<div class="progress-bar" role="progressbar" style="width:'+obj[item]+'%;" aria-valuenow="'+obj[item]+'" aria-valuemin="0" aria-valuemax="4">'+item+'</div>';
+         content2 +='<div class="progress-bar" role="progressbar" style="width:20%;" aria-valuenow="'+obj[item]+'" aria-valuemin="0" aria-valuemax="4">'+item+'</div>';
          content2 +='</div>';
+         content2 +='<br>';
+
         // console.log(item)
         // console.log(obj[item])
 
