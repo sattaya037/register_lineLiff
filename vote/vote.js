e7906f223d43dceb1da76ca644ba99d9cd5cdded
sattaya037 <sattaya.ra@ics-th.com>
2019-12-30 12:03:56 +0700

    make it better

--- vote/vote.js
+++ vote/vote.js
@@ -224,8 +224,8 @@ function render(arrObj){
 
 function onSubmit(snapKey,liClass,uid){
     var element = document.getElementById(liClass);
-    document.getElementsByClassName("list-group-item").className = "list-group-item";
-
+    var elems = document.querySelectorAll("list-group-item active");
+    console.log(elems)
     element.className = element.className+' active';
         
     const voteRef = firebase.database().ref('HPY/voters')
